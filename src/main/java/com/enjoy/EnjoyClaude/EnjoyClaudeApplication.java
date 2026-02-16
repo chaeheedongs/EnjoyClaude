1c0c31f1c505fa4dedeff368ee72b6a5e4ab26bc
chaeheedongs <qiip91@gmail.com>
2026-02-16 11:08:55 +0900

    - init

---
+++ src/main/java/com/enjoy/EnjoyClaude/EnjoyClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.enjoy.EnjoyClaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyClaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enjoy/EnjoyClaude/EnjoyClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoy.EnjoyClaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyClaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
